20753a1c0cdb44e3ee942c63992a5390ce0a4e0f
Porkenvolf <89871231+porkenvolf@users.noreply.github.com>
2023-05-09 21:18:32 -0300

    Implement Clear and Slider functionality

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector("#container");
+const colorBtn = document.querySelector("#color");
+const rainbowBtn = document.querySelector("#rainbow");
+const clearBtn = document.querySelector("#clear");
+const slider = document.querySelector("#slider");
 
 function newGrid(squaresPerSide) {
     container.style.cssText = "flex: 0 1 60vh;";
@@ -15,4 +19,16 @@ function newGrid(squaresPerSide) {
         container.appendChild(tempDiv);
     }
 }
-newGrid(15);
+
+function clear() {
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach((item) => {
+        item.remove();
+    });
+    newGrid(slider.value);
+}
+clearBtn.addEventListener("click", clear);
+slider.addEventListener("change", (event) => {
+    clear();
+});
+newGrid(slider.value);
